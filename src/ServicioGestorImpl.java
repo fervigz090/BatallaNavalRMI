d368bbd33dd5ed84d3d18d477e3a00f8a37f095d
Ivan Fernandez <ivan@M1-Pro.local>
2023-10-31 18:50:47 +0100

    first commit

--- src/ServicioGestorImpl.java
+++ src/ServicioGestorImpl.java
@@ -1,2 +1,7 @@
+/**
+ * Este servicio se encarga de gestionar todas las operaciones de los jugadores. Crea
+ * todas las estructuras de datos necesarias para jugar la partida, gestiona la
+ * logica de juego, informa a los jugadores del fin de partida, puntuiaciones, etc.
+ */
 public class ServicioGestorImpl {
 }
