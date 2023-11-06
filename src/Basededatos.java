6c7e70fd1405bec64434dd88ee9f9fe594c56a47
Ivan Fernandez <ivan@M1-Pro.local>
2023-11-06 18:03:09 +0100

    implementacion base de datos

--- src/Basededatos.java
+++ src/Basededatos.java
@@ -1,2 +1,52 @@
+import java.util.HashMap;
+
 public class Basededatos {
+
+    private HashMap<String, String> RegistroUsuarios = new HashMap<>();
+
+    public Basededatos(){
+
+    }
+
+    /**
+     * Almacena un jugador en RegistroUsuarios
+     * @param name String
+     * @param password String
+     */
+    public void setUser(String name, String password){
+        RegistroUsuarios.put(name, password);
+    }
+
+    /**
+     * Devuelve True si el nombre introducido lo está usando ya otro jugador
+     * @param name String
+     * @return boolean
+     */
+    public boolean existe(String name){
+        return RegistroUsuarios.containsKey(name);
+    }
+
+    /**
+     * Devuelve la contraseña del usuario introducido como parametro.
+     * @param name String
+     * @return String
+     */
+    public String getPass(String name){
+        if(existe(name)){
+            return RegistroUsuarios.get(name);
+        }else return null;
+    }
+
+    /**
+     * Elimina el jugador de RegistroUsuarios.
+     * @param name String
+     */
+    public void deleteUser(String name){
+        if(existe(name)){
+            RegistroUsuarios.remove(name);
+        }
+    }
+
+
+
 }
