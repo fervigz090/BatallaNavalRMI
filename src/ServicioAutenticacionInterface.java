ab81a3f122cdd0e6d357c2b6eb551e11dcb96999
Ivan Fernandez <ivan@M1-Pro.local>
2023-11-16 10:07:13 +0100

    implementacion interfaz autenticacion

--- src/ServicioAutenticacionInterface.java
+++ src/ServicioAutenticacionInterface.java
@@ -1,2 +1,10 @@
-public class ServicioAutenticacionInterface {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServicioAutenticacionInterface extends Remote {
+
+    boolean login(String name, String password) throws RemoteException;
+
+    void registrar(String name, String password) throws RemoteException;
+
 }
