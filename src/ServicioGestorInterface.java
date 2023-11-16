7c7917912c8862a7e686e28327c784fbd2b40702
Ivan Fernandez <ivan@M1-Pro.local>
2023-11-16 11:13:28 +0100

    implementacion interfaz gestor

--- src/ServicioGestorInterface.java
+++ src/ServicioGestorInterface.java
@@ -1,2 +1,19 @@
-public class ServicioGestorInterface {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServicioGestorInterface extends Remote {
+
+    Tablero crearTablero() throws RemoteException;
+
+    void colocarBarcos(Jugador jugador, Tablero tablero) throws RemoteException;
+
+    void iniciarPartida(Jugador jugador1, Jugador jugador2,
+                        Tablero tablero1, Tablero tablero2) throws RemoteException;
+
+    void colocarFicha(Jugador jugador, Tablero tablero, char letra, char numero) throws RemoteException;
+
+    boolean partidaFinalizada(Tablero tablero1, Tablero tablero2) throws RemoteException;
+
+
+
 }
