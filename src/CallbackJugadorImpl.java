43e3b247e94fac0237aaf998709ac800d2b77ef7
Ivan Fernandez <ivan@M1-Pro.local>
2023-11-22 17:55:22 +0100

    Implementando callbackjugadorimpl

--- src/CallbackJugadorImpl.java
+++ src/CallbackJugadorImpl.java
@@ -1,2 +1,28 @@
-public class CallbackJugadorImpl {
+import java.rmi.RemoteException;
+
+public class CallbackJugadorImpl implements CallbackJugadorInterface{
+    @Override
+    public void partidaIniciada() throws RemoteException {
+
+    }
+
+    @Override
+    public void turnoParaDisparar() throws RemoteException {
+
+    }
+
+    @Override
+    public void resultadoDisparoOponente(boolean impacto, boolean hundido, char fila, int columna) throws RemoteException {
+
+    }
+
+    @Override
+    public void hasGanado() throws RemoteException {
+
+    }
+
+    @Override
+    public void hasPerdido() throws RemoteException {
+
+    }
 }
--- src/CallbackJugadorInterface.java
+++ src/CallbackJugadorInterface.java
@@ -1,2 +1,16 @@
-public class CallbackJugadorInterface {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface CallbackJugadorInterface extends Remote {
+
+    void partidaIniciada() throws RemoteException;
+
+    void turnoParaDisparar() throws RemoteException;
+
+    void resultadoDisparoOponente(boolean impacto, boolean hundido, char fila, int columna) throws RemoteException;
+
+    void hasGanado() throws RemoteException;
+
+    void hasPerdido() throws RemoteException;
+
 }
